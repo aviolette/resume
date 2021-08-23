c26775c10a3a43a739003fbd3624905c4e0feb05
Andrew Violette <andrew@andrewviolette.net>
2021-08-23 09:08:30 -0500

    Named file appropriately

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def convert_resume():
         _add_contact(resume_config, pdf)
         _add_skills(resume_config, pdf)
         _add_projects(resume_config, pdf)
-    pdf.output("resume.pdf", "F")
+    pdf.output("AJVResume.pdf", "F")
 
 
 def _add_header(resume_config, pdf):
